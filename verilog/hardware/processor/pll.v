580305ede8d4d1ef41fde0bab2edf4d76c0e6697
Algorhythm-sxv <sebastian.venter@gmail.com>
2021-05-30 19:59:35 +0000

    boost PLL clock to 14.0625MHz

--- verilog/hardware/processor/pll.v
+++ verilog/hardware/processor/pll.v
@@ -1,21 +1,21 @@
 module pll(
-	input  clock_in,
-	output clock_out,
-	);
+        input  clock_in,
+        output clock_out,
+        );
 
 SB_PLL40_CORE #(
-		.FEEDBACK_PATH("SIMPLE"),
-		.DIVR(4'b0000),		// DIVR =  0
-		.DIVF(7'b1000111),	// DIVF = 71
-		.DIVQ(3'b101),		// DIVQ =  5
-		.FILTER_RANGE(3'b001)	// FILTER_RANGE = 1
-	) uut (
-		.LOCK(),
-		.RESETB(1'b1),
-		.BYPASS(1'b0),
-		.REFERENCECLK(clock_in),
-		.PLLOUTCORE(clock_out)
-		);
+                .FEEDBACK_PATH("SIMPLE"),
+                .DIVR(4'b0000),         // DIVR =  0
+                .DIVF(7'b1001010),      // DIVF = 74
+                .DIVQ(3'b101),          // DIVQ =  5
+                .FILTER_RANGE(3'b001)   // FILTER_RANGE = 1
+        ) uut (
+                .LOCK(),
+                .RESETB(1'b1),
+                .BYPASS(1'b0),
+                .REFERENCECLK(clock_in),
+                .PLLOUTCORE(clock_out)
+                );
 
 endmodule
 
